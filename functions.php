e475320220485816106b4abf6b3687225f13e2f2
Kharis Sulistiyono <kharisblank@gmail.com>
2018-06-01 09:36:57 +0700

    Remove custom post clasS

--- functions.php
+++ functions.php
@@ -12,3 +12,11 @@ function talon_child_enqueue() {
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
 
 }
+
+/**
+ * Remove custom post class
+ */
+add_action('init', 'talon_child_remove_custom_post_class');
+function talon_child_remove_custom_post_class() {
+  remove_filter( 'post_class', 'talon_post_classes' );
+}
